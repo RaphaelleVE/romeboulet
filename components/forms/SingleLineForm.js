ca600cfc161eaf86bc08e6080d540b66b027ed71
Raphaelle Verdiere <Raphaelle.verdiere-cic@ibm.com>
2023-12-06 11:26:40 +0100

    feat : 6/11

---
+++ components/forms/SingleLineForm.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import { StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import AppTextInput from "../AppTextInput";
+import ErrorMessage from "./ErrorMessage";
+import AppButton from "../AppButton";
+import { FaSave } from "react-icons/fa";
+
+
+
+
+function AppFormField({ name, width, ...otherProps }) {
+  const {
+    setFieldTouched,
+    setFieldValue,
+    errors,
+    touched,
+    values,
+  } = useFormikContext();
+
+  const [loaded] = useFonts({
+    Marhey: require('../../assets/fonts/Marhey-Light.ttf')
+  })
+
+  if(!loaded) {return null}
+
+  if (loaded) {
+    return (
+      <>
+        <AppTextInput
+          onBlur={() => setFieldTouched(name)}
+          onChangeText={(text) => setFieldValue(name, text)}
+          value={values[name]}
+          width={width}
+          {...otherProps}
+        />
+                  <FaSave />
+
+        <AppButton source={require("../../assets/bg-login.png")}>
+          <FaSave />
+</AppButton>
+        <ErrorMessage error={errors[name]} visible={touched[name]} style={ styles.text } />
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'Marhey',
+    fontSize: 16,
+    flex: 1
+  },
+});
+
+export default AppFormField;
