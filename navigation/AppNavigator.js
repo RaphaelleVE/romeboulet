86c719df8b714a30b5e4e7fa22fb6ca65b55402a
Raphaelle Verdiere <Raphaelle.verdiere-cic@ibm.com>
2023-12-12 23:08:13 +0100

    feat : prettier & opti appbar

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { StyleSheet, Image, ImageBackground } from "react-native";
 
 
-import ShoppingNavigator from "./ShoppingNavigator";
 import ProfileScreen from "../screens/ProfileScreen";
 import CartScreen from "../screens/CartScreen";
+import ShoppingManagerScreen from "../screens/ShoppingManagerScreen";
 
 
 import NewListingButton from "./NewListingButton";
 import routes from "./routes";
 import colors from "../config/colors";
-import navigationTheme from "./navigationTheme";
-import ShoppingManagerScreen from "../screens/ShoppingManagerScreen";
 
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator
-  theme={appTheme}
   screenOptions={{
     headerShown: false,
   }}>
@@ -28,30 +25,35 @@ const AppNavigator = () => (
       name="Profile"
       component={ProfileScreen}
       options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
+        tabBarIcon: ({ color, focused , size }) => (
+          <MaterialCommunityIcons 
+          name="account" 
+          color={focused ? colors.black : colors.mainBrown} 
+          size={size}  />
         ),
       }}
     />
     <Tab.Screen
-      name="Shopping"
+      name="ShoppingManagerScreen"
       component={ShoppingManagerScreen}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons
-            name="cart-plus"
-            color={colors.mainBrown}
-            size={size}
+      options={({ navigation }) => ({
+        tabBarButton: (useIsFocused) => (
+          <NewListingButton
+            onPress={() => navigation.navigate(routes.SHOPPINGMANAGERSCREEN)}
+            styleParam={useIsFocused ? null : styles.buttonFocused}
           />
         ),
-      }}
+      })}
     />
       <Tab.Screen
       name="Cart"
       component={CartScreen}
       options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
+        tabBarIcon: ({ color, size , focused}) => (
+          <MaterialCommunityIcons 
+          name="basket" 
+          size={size} 
+          color={focused ? colors.black : colors.mainBrown} />
         ),
       }}
     />
@@ -59,13 +61,12 @@ const AppNavigator = () => (
 );
 
 
-const appTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background:"../assets/bg-moche.png"
-  },
-};
+const styles = StyleSheet.create({
+  buttonFocused: {
+    backgroundColor: colors.black,
+    borderColor: colors.mainWhite,
+    }
+});
 
 
 export default AppNavigator;
--- screens/MainPagesScreen.js
+++ screens/MainPagesScreen.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+
 
 import AppNavigator from "../navigation/AppNavigator";
 
 function MainPagesScreen({navigation}) {
   return (
-      <AppNavigator />
+      <AppNavigator />    
   );
 }
-
 
 export default MainPagesScreen;
 
