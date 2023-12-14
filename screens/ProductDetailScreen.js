753fd73d2dc74b739fbd9d5e638e94dc7b96af2d
Raphaelle Verdiere <Raphaelle.verdiere-cic@ibm.com>
2023-12-14 17:24:41 +0100

    TO FINISH JSON

--- screens/ProductDetailScreen.js
+++ screens/ProductDetailScreen.js
@@ -5,9 +5,59 @@ import colors from "../config/colors";
 import AppText from "../components/AppText";
 import AppButton from "../components/AppButton";
 import routes from "../navigation/routes";
+import * as cartData from '../test.json';
+
+
+const listings = [
+  {
+    id: 1,
+    title: "Tonneaux de Grog",
+    price: 100,
+    description: "Délicieux tonneaux de grog issue de la réserve personelle du capitaine Pendragon",
+    image: require("../assets/baril_Grog.png"),
+  },
+  {
+    id: 2,
+    title: "Boulets de canon",
+    price: 10,
+    description: "De bons boulets (je ne parle pas de vos matelots) utile pour casser du navire",
+    image: require("../assets/cannon_ball.png"),
+  },
+  {
+    id: 3,
+    title: "Noix de Coco",
+    price: 2,
+    description: "Noix de coco, idéale pour se raffraichir en pleine mer! a consommer avec modération",
+    image: require("../assets/coconut.png"),
+  },
+  {
+    id: 4,
+    title: "Bananes",
+    price: 2,
+    description: "Le véritable snack du bon pirate, permet de se nourrir peu importe la situation",
+    image: require("../assets/banana.jpg"),
+  },
+  {
+    id: 5,
+    title: "Bouteilles de Rhum",
+    price: 20,
+    description: "caisse de bouteille de rhum du sailor's bounty, a consommer sans aucune modération ! ",
+    image: require("../assets/rhum_Bottles.png"),
+  }];
 
 function ProductDetailScreen({navigation, route}) {
    let [quantity, setQuantity] = useState(0);
+
+   function handleAddToListings () {
+    try {
+      data=cartData.cart
+      data.push(listings)
+
+    } catch (error) {
+      console.error("Erreur lors de l'ajout de l'élément au fichier JSON :", error);
+    }
+  };
+
    function incrementQuantity() {
      quantity = quantity + 1;
      setQuantity(quantity);
@@ -35,7 +85,7 @@ function ProductDetailScreen({navigation, route}) {
             <AppButton styleParam={styles.buttons} customTitle="+" onPress={incrementQuantity}></AppButton>
           </View>
           <View style={styles.orderButton}>
-          <AppButton styleParam={styles.orderButton} customTitle="Ajouter au Panier"></AppButton>
+          <AppButton styleParam={styles.orderButton} customTitle="Ajouter au Panier" onPress={handleAddToListings}></AppButton>
           </View>
           </ScrollView>
       </ImageBackground>
